dce2c882cd1c439dd58482b7d103f1bbfa805d47
John Moustakas <john.moustakas@gmail.com>
2022-01-12 11:56:37 -0500

    link to v3.2 basis templates

--- setup-testdata.sh
+++ setup-testdata.sh
@@ -4,5 +4,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export DESI_ROOT=$DIR/desi
-export DESI_BASIS_TEMPLATES=$DESI_ROOT/spectro/templates/basis_templates/v3.0
+export DESI_BASIS_TEMPLATES=$DESI_ROOT/spectro/templates/basis_templates/v3.2
 
